241e7598c39cc8c1e5d1172430c301432a4ccf19
Radio <radio@bemaniso.ws>
2024-04-01 20:15:20 +0200

    Prepare next iteration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "1.6"
+        versionName = "1.7-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
