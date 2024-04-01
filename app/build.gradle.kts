cbb306e584b91f98775f2ebd1653d7a0e9ae0a72
Radio <radio@bemaniso.ws>
2024-04-01 19:30:38 +0200

    Prepare release 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "1.5"
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
