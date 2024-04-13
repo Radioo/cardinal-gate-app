ddeb1fcd0f700752f54f6325c9338bfe5e606f75
Radio <radio@bemaniso.ws>
2024-04-13 16:35:23 +0200

    Push tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "1.7.1"
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
